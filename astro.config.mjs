c1357d53bf1c6d098540d4f9df0a214d271198d5
David Cordero <daco.aray@gmail.com>
2026-07-18 14:09:00 -0600

    feat: Make site static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: process.env.SITE ?? "https://atelier-ko-topaz.vercel.app",
-  output: "server",
-  adapter: vercel(),
+  output: "static",
   integrations: [sitemap()],
 });
