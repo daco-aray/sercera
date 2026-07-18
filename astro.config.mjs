b49cc9fff24d51e155eff82d65ee6855169ebbdd
David Cordero <daco.aray@gmail.com>
2026-07-18 09:53:04 -0600

    feat: Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: process.env.SITE ?? "https://atelier-ko-topaz.vercel.app",
   output: "server",
+  adapter: vercel(),
   integrations: [sitemap()],
 });
